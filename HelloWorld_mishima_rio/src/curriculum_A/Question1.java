e4256b43ad116240b38cdcb638461fed30da9a4f
User <User@DESKTOP-KSTL47E>
2025-12-12 01:54:59 +0900

    2.8 変数②

--- HelloWorld_mishima_rio/src/curriculum_A/Question1.java
+++ HelloWorld_mishima_rio/src/curriculum_A/Question1.java
@@ -63,6 +63,67 @@ public class Question1 {
         System.out.println("letter = " + letter);
         System.out.println("letters = " + letters);
         System.out.println("isBoolean = " + isBoolean);
+        
+        // Q2の値を代入した変数を用いて出力
+        
+        // 11110 → 10 + 100 + 1000 + 10000
+        System.out.println(byteNum + shortNum + intNum + longNum);
+
+        // 20 → 10 + 10
+        System.out.println(byteNum + byteNum);
+
+        // a ハロー true（半角スペース入れる）
+        System.out.println(letter + " " + letters + " " + isBoolean);
+
+        // 11130 → 11110 + 20
+        System.out.println(byteNum +byteNum + byteNum +  shortNum + intNum + longNum );
+
+        // 1E+10 小数点以外の数字をすべてかける
+        long result = (long) byteNum * shortNum * intNum * longNum;
+        System.out.println(result); 
+        
+        // 0.105 → 10.5 / 100
+        System.out.println(doubleNum / 100);
+
+        // -90 → 10 - 100
+        System.out.println(byteNum - shortNum);
+        
+        
+        // String型の変数 name を宣言して値を代入
+        String name = "山田太郎";
+        
+        // name を使って出力
+        System.out.println("こんにちは、" + name + "さん!");
+        
+        
+        // int型の変数 age を宣言して値を代入
+        int age = 25;
+
+        // age を使って出力
+        System.out.println("年齢: " + age + "歳");
+        
+        
+        // 変数の宣言と代入
+        int num1 = 10;
+        int num2 = 5;
+
+        // 足した結果を sum に代入
+        int sum = num1 + num2;
+
+        // 結果を出力
+        System.out.println(sum);
+        
+        
+        // 変数 score を宣言して値を代入
+        int score = 80;
+
+        // score に 20 を加える
+        score = score + 20;
+
+        // 最終スコアを出力
+        System.out.println("最終スコア: " + score);
+
+
     }
 }
 
