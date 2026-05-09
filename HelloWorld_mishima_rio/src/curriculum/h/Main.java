e090c1cea428e98d93decaa59fd094faf82a4ff8
User <User@DESKTOP-KSTL47E>
2026-05-09 23:39:46 +0900

    課題2.22 オブジェクト指向まとめ

--- HelloWorld_mishima_rio/src/curriculum/h/Main.java
+++ HelloWorld_mishima_rio/src/curriculum/h/Main.java
@@ -7,18 +7,20 @@ public class Main {
         // 正社員
         FullTimeEmployee full = new FullTimeEmployee("E001", "佐藤太郎");
 
-        // パート
+        // パート社員
         PartTimeEmployee part = new PartTimeEmployee("E002", "田中花子");
 
         // 9時間勤務の給与
         int fullWage = full.calculateDailyWage(9);
         int partWage = part.calculateDailyWage(9);
 
-        // 出力
-        System.out.println("社員ID: E001, 名前: 佐藤太郎");
+        // getterを使って出力
+        System.out.println("社員ID: " + full.getEmployeeId()
+                + ", 名前: " + full.getName());
         System.out.println("正社員の給与: " + fullWage + " 円");
 
-        System.out.println("社員ID: E002, 名前: 田中花子");
+        System.out.println("社員ID: " + part.getEmployeeId()
+                + ", 名前: " + part.getName());
         System.out.println("パート社員の給与: " + partWage + " 円");
     }
 }
